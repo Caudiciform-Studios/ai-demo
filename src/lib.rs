810fa55dbb8f7c0cb20fcdf5b17a7ddd4254baed
Alec Deason <alec@tinycountry.com>
2024-09-11 11:48:02 -0700

    Get flag guarding working

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,8 @@ use serde::{Deserialize, Serialize};
 use bindings::{game::auto_rogue::types::MicroAction, *};
 
 use client_utils::{
-    behaviors::convert,
-    crdt::CrdtContainer,
+    behaviors::*,
+    crdt::{CrdtContainer, ExpiringFWWRegister, SizedFWWExpiringSet},
     find_action,
     framework::{ExplorableMap, State},
 };
@@ -13,11 +13,27 @@ use client_utils::{
 #[derive(Default, Serialize, Deserialize)]
 struct Memory {
     broadcast: Broadcast,
+    home_level: Option<i64>,
 }
 
-#[derive(Default, Debug, Serialize, Deserialize, CrdtContainer)]
+#[derive(Debug, Serialize, Deserialize, CrdtContainer)]
 struct Broadcast {
+    #[crdt]
     map: ExplorableMap,
+    #[crdt]
+    dungeoneer: ExpiringFWWRegister<i64>,
+    #[crdt]
+    flag_guards: SizedFWWExpiringSet<i64>,
+}
+
+impl Default for Broadcast {
+    fn default() -> Self {
+        Self {
+            map: ExplorableMap::default(),
+            dungeoneer: ExpiringFWWRegister::default(),
+            flag_guards: SizedFWWExpiringSet::new(3),
+        }
+    }
 }
 
 impl State<Broadcast, ExplorableMap> for Memory {
@@ -30,47 +46,151 @@ impl State<Broadcast, ExplorableMap> for Memory {
     }
 
     fn run(&mut self) -> Command {
+        let current_level = get_game_state().level_id;
+        if self.home_level.is_none() {
+            self.home_level = Some(current_level);
+        }
+
+        let (current_loc, actor) = actor();
+        let now = get_game_state().turn;
+        let mut is_dungeoneer = false;
+        let mut is_guard = self.broadcast.flag_guards.contains(&actor.id);
+
+        if !is_guard {
+            if let Some(dungeoneer) = self.broadcast.dungeoneer.get() {
+                if *dungeoneer == actor.id {
+                    is_dungeoneer = true;
+                    self.broadcast.dungeoneer.update_expiry(now + 500);
+                }
+            } else {
+                self.broadcast.dungeoneer.set(now, now + 500, actor.id);
+            }
+        }
+
         let inventory = inventory();
         let inventory_full =
             get_character_stats().current.inventory_size as usize <= inventory.len();
         let mut coin_count = 0;
         let mut fruit_count = 0;
+        let mut has_weapon = false;
+        let mut extra_weapons = vec![];
         for i in &inventory {
             if i.name == "Coin" {
                 coin_count += 1;
             } else if i.name == "Fruit" {
                 fruit_count += 1;
+            } else if find_action!(MicroAction::Attack { .. }, i).is_some() {
+                if get_equipment_state().right_hand.is_none() {
+                    if let Some(command) = equip(i.id, EquipmentSlot::RightHand) {
+                        return command
+                    }
+                }
+                if get_equipment_state().right_hand != Some(i.id) {
+                    extra_weapons.push(i.id);
+                } else {
+                    has_weapon = true;
+                }
             }
         }
-        let (current_loc, _) = actor();
 
-        if let Some(command) = convert() {
-            return command;
+        if has_weapon && !is_dungeoneer {
+            self.broadcast.flag_guards.insert(actor.id, now, now + 500);
+            is_guard = self.broadcast.flag_guards.contains(&actor.id);
         }
 
-        if !inventory_full {
-            if let Some(item) = item_at(current_loc) {
-                if !item.is_furniture
-                    && let Some((id, _action, _micro_action)) = find_action!(MicroAction::PickUp)
+
+        if self.home_level == Some(current_level) {
+            if is_dungeoneer {
+                if !extra_weapons.is_empty() {
+                    if let Some((id, _, _)) = find_action!(MicroAction::Drop) {
+                        return Command::UseAction((id as u32, Some(ActionTarget::Items(extra_weapons))));
+                    }
+                }
+                if let Some(command) = self.broadcast.map.move_towards_nearest(&["Exit"]) {
+                    return command;
+                }
+            } else if is_guard {
+                if let Some(command) = attack_nearest() {
+                    return command;
+                }
+
+                if let Some(command) = self.broadcast.map.move_towards_nearest(&["Flag"]) {
+                    return command;
+                }
+            } else {
+                if let Some(command) = attack_nearest() {
+                    return command;
+                }
+                if !inventory_full {
+                    if !has_weapon {
+                        if let Some(command) = self.broadcast.map.move_towards_nearest(&["Bow", "Sword"]) {
+                            return command;
+                        }
+                    }
+                    if let Some(item) = item_at(current_loc) {
+                        if !item.is_furniture
+                            && (["Coin", "Fruit"].contains(&item.name.as_str())
+                            || (!has_weapon && ["Bow", "Sword"].contains(&item.name.as_str())))
+                            && let Some((id, _action, _micro_action)) = find_action!(MicroAction::PickUp)
+                        {
+                            return Command::UseAction((id as u32, None));
+                        }
+                    }
+                }
+                if let Some(command) = convert() {
+                    return command;
+                }
+
+                if !inventory_full {
+                    if let Some(command) = self.broadcast.map.move_towards_nearest(&["Coin", "Fruit"]) {
+                        return command;
+                    }
+                } else if coin_count > 0
+                    && let Some(command) = self.broadcast.map.move_towards_nearest(&["Flag"])
                 {
-                    return Command::UseAction((id as u32, None));
+                    return command;
+                } else if fruit_count > 0
+                    && let Some(command) = self.broadcast.map.move_towards_nearest(&["Shrine"])
+                {
+                    return command;
+                }
+            }
+        } else if !is_dungeoneer {
+            if let Some(command) = self.broadcast.map.move_towards_nearest(&["Exit"]) {
+                return command;
+            }
+        } else {
+            if !inventory_full {
+                if let Some(item) = item_at(current_loc) {
+                    if !item.is_furniture
+                        && let Some((id, _action, _micro_action)) = find_action!(MicroAction::PickUp)
+                    {
+                        return Command::UseAction((id as u32, None));
+                    }
+                }
+            }
+            if self.broadcast.map.nearest(&["Coin"]).is_some() {
+                if let Some(command) = self.broadcast.map.move_towards_nearest(&["Exit"]) {
+                    return command;
+                }
+            } else {
+                if let Some(command) = self.broadcast.map.move_towards_nearest(&["Special"]) {
+                    return command;
+                }
+
+                if let Some(command) = self.broadcast.map.move_towards_nearest(&["Bow", "Sword"]) {
+                    return command;
                 }
             }
 
-            if let Some(command) = self.broadcast.map.move_towards_nearest(&["Coin", "Fruit"]) {
+            if let Some(command) = self.broadcast.map.move_towards_nearest(&["Exit"]) {
                 return command;
             }
-        } else if coin_count > 0
-            && let Some(command) = self.broadcast.map.move_towards_nearest(&["Flag"])
-        {
-            return command;
-        } else if fruit_count > 0
-            && let Some(command) = self.broadcast.map.move_towards_nearest(&["Shrine"])
-        {
-            return command;
         }
 
         if let Some(command) = self.broadcast.map.explore() {
+            command
+        } else if let Some(command) = wander() {
             command
         } else {
             Command::Nothing
