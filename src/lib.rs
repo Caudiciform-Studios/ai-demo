f50d5e97b0a22508eb323afca695b92ebf637b4f
Alec Deason <alec@tinycountry.com>
2024-09-15 02:28:36 -0700

    work on creature highlights and other stuff

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #![feature(let_chains)]
 use serde::{Deserialize, Serialize};
 
-use bindings::{game::auto_rogue::types::MicroAction, *};
+use bindings::{game::auto_rogue::types::{ApplyBuffParams, MicroAction}, *};
 
 use client_utils::{
     behaviors::*,
@@ -84,6 +84,8 @@ impl State<Broadcast, ExplorableMap> for Memory {
         let mut coin_count = 0;
         let mut fruit_count = 0;
         let mut wielded_weapon = None;
+        let mut wielded_shield = None;
+        let mut wielded_wand = None;
         for i in &inventory {
             if i.name == "Coin"{
                 coin_count += 1;
@@ -91,6 +93,24 @@ impl State<Broadcast, ExplorableMap> for Memory {
                 fruit_count += 1;
             } else if i.name == "Gem" {
                 coin_count += 1;
+            } else if i.name == "Shield" {
+                if get_equipment_state().left_hand.is_none() {
+                    if let Some(command) = equip(i.id, EquipmentSlot::LeftHand) {
+                        return command
+                    }
+                }
+                if get_equipment_state().left_hand == Some(i.id) {
+                    wielded_shield = Some(i.id);
+                }
+            } else if i.name == "Wand of Haste" {
+                if get_equipment_state().left_hand.is_none() {
+                    if let Some(command) = equip(i.id, EquipmentSlot::LeftHand) {
+                        return command
+                    }
+                }
+                if get_equipment_state().left_hand == Some(i.id) {
+                    wielded_wand = Some(i.id);
+                }
             } else if find_action!(MicroAction::Attack { .. }, i).is_some() {
                 if get_equipment_state().right_hand.is_none() {
                     if let Some(command) = equip(i.id, EquipmentSlot::RightHand) {
@@ -113,9 +133,52 @@ impl State<Broadcast, ExplorableMap> for Memory {
         }
 
 
+        if is_dungeoneer {
+            highlight_actor(Some((0.4, 0.0, 0.0)));
+        } else if is_dedicated_scorer {
+            highlight_actor(Some((0.0, 0.4, 0.0)));
+        } else if is_flag_guard {
+            highlight_actor(Some((0.0, 0.0, 0.4)));
+        } else if is_stair_guard {
+            highlight_actor(Some((0.0, 0.4, 0.8)));
+        } else {
+            highlight_actor(None);
+        }
+
+        println!("{now} dungeoneer: {is_dungeoneer} dedicated scorer: {is_dedicated_scorer} flag guard: {is_flag_guard} stair guard: {is_stair_guard}");
+
+
+        if let Some((id, _, MicroAction::ApplyBuff(ApplyBuffParams { range, ..}))) = find_action!(MicroAction::ApplyBuff(ApplyBuffParams { name, ..}) if name == "haste") {
+            for (loc, creature) in visible_creatures().iter().chain(&[(current_loc, actor.clone())]) {
+                if creature.faction == actor.faction && !is_hasted(creature) {
+                    let d = distance(*loc, current_loc);
+                    if d <= range as f32 {
+                        return Command::UseAction((id as u32, Some(ActionTarget::Creature(creature.id))));
+                    }
+                }
+            }
+
+        }
+
+        if is_dungeoneer && wielded_shield.is_some() {
+            let mut defense = 0;
+            for buff in character_buffs() {
+                if buff.name == "defense" {
+                    defense += buff.amount;
+                    break
+                }
+            }
+
+            if defense < 40 {
+                if let Some((id, _, _)) = find_action!(MicroAction::ApplyBuff(ApplyBuffParams { name, ..}) if name == "defense") {
+                    return Command::UseAction((id as u32, Some(ActionTarget::Actor)));
+                }
+            }
+        }
+
         if self.home_level == Some(current_level) {
             if is_dungeoneer {
-                let baggage: Vec<_> = inventory.iter().map(|i| i.id).filter(|i| Some(*i) != wielded_weapon).collect();
+                let baggage: Vec<_> = inventory.iter().map(|i| i.id).filter(|i| Some(*i) != wielded_weapon && Some(*i) != wielded_shield).collect();
                 if !baggage.is_empty() {
                     if let Some((id, _, _)) = find_action!(MicroAction::Drop) {
                         return Command::UseAction((id as u32, Some(ActionTarget::Items(baggage))));
@@ -150,18 +213,32 @@ impl State<Broadcast, ExplorableMap> for Memory {
                     }
                 }
                 if !inventory_full {
-                    if wielded_weapon.is_none() && !is_dedicated_scorer {
-                        if let Some(command) = self.broadcast.map.move_towards_nearest(&["Bow", "Sword"]) {
-                            return command;
-                        }
-                    }
                     if let Some(item) = item_at(current_loc) {
-                        if !item.is_furniture
-                            && (["Gem", "Coin", "Fruit"].contains(&item.name.as_str())
-                            || (wielded_weapon.is_none() && ["Bow", "Sword"].contains(&item.name.as_str())))
+                        let mut goals = vec!["Gem", "Coin", "Fruit"];
+                        if wielded_weapon.is_none() {
+                            goals.extend(["Bow", "Sword"]);
+                        }
+                        if wielded_wand.is_none() {
+                            goals.extend(["Wand of Haste"]);
+                        }
+                        println!("pickup {}", goals.contains(&item.name.as_str()));
+                        if goals.contains(&item.name.as_str())
                             && let Some((id, _action, _micro_action)) = find_action!(MicroAction::PickUp)
                         {
                             return Command::UseAction((id as u32, None));
+                        }
+                    }
+
+                    if !is_dedicated_scorer {
+                        if wielded_weapon.is_none() {
+                            if let Some(command) = self.broadcast.map.move_towards_nearest(&["Bow", "Sword"]) {
+                                return command;
+                            }
+                        }
+                        if wielded_wand.is_none() {
+                            if let Some(command) = self.broadcast.map.move_towards_nearest(&["Wand of Haste"]) {
+                                return command;
+                            }
                         }
                     }
                 }
@@ -202,12 +279,12 @@ impl State<Broadcast, ExplorableMap> for Memory {
                 if let Some(command) = attack_nearest(&[actor.faction, 0]) {
                     return command;
                 }
-                if self.broadcast.pop_boosts.len() < 3 {
+                if self.broadcast.pop_boosts.len() <= 0 {
                     if let Some(command) = self.broadcast.map.move_towards_nearest(&["Special", "Exit"]) {
                         return command;
                     }
                 } else {
-                    if let Some(command) = self.broadcast.map.move_towards_nearest(&["Special", "Bow", "Sword", "Exit"]) {
+                    if let Some(command) = self.broadcast.map.move_towards_nearest(&["Special", "Bow", "Sword", "Shield", "Wand of Haste", "Exit"]) {
                         return command;
                     }
                 }
@@ -229,6 +306,16 @@ impl State<Broadcast, ExplorableMap> for Memory {
             Command::Nothing
         }
     }
+}
+
+
+fn is_hasted(creature: &Creature) -> bool {
+    for buff in &creature.buffs {
+        if buff.name == "haste" && buff.amount > 10 {
+            return true
+        }
+    }
+    false
 }
 
 type Component = client_utils::framework::Component<Memory, Broadcast, ExplorableMap>;
